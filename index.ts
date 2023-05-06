8c1d16744ff4ef9def65a1b00c777433d96b9341
Awais Khalil <126592168+Awais-Khalil@users.noreply.github.com>
2023-05-06 18:17:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import fs from 'fs';
+
+function countWordsAndCharacters(filePath: string): Promise<{ words: number, characters: number }> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        const words = data.trim().split(/\s+/).length;
+        const characters = data.replace(/\s+/g, '').length;
+        resolve({ words, characters });
+      }
+    });
+  });
+}
+
+// Example usage
+const filePath = './myParagraph.txt';
+countWordsAndCharacters(filePath)
+  .then(result => {
+    console.log(`Number of words: ${result.words}`);
+    console.log(`Number of characters (without whitespaces): ${result.characters}`);
+  })
+  .catch(error => {
+    console.error(`Error reading file: ${error}`);
+  });
